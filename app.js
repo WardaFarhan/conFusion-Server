e2e43896d3a2d9140889977ecd82a7ded651d845
WardaFarhan <farhan.warda337@gmail.com>
2020-10-08 15:03:32 +0530

    Express Session Part 1

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
 var indexRouter = require('./routes/indexRouter');
 var usersRouter = require('./routes/usersRouter');
@@ -31,16 +33,26 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
+//app.use(cookieParser('12345-67890-09876-54321'));
+
+app.use(session({
+  name: 'session-is',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
 
 function auth (req, res, next) {
   //console.log(req.headers);
-  console.log(req.signedCookies);
+  //console.log(req.signedCookies);
+  console.log(req.session)
 
-  if(!req.signedCookies.user){
-  var authHeader = req.headers.authorization;
+  //if(!req.signedCookies.user){
+  if(!req.session.user){
+   var authHeader = req.headers.authorization;
   
-  if (!authHeader) {
+   if (!authHeader) {
       var err = new Error('You are not authenticated!');
       res.setHeader('WWW-Authenticate', 'Basic');
       err.status = 401;
@@ -52,7 +64,8 @@ function auth (req, res, next) {
   var user = auth[0];
   var pass = auth[1];
   if (user == 'admin' && pass == 'password') {
-    res.Cookie('user', 'admin', {signed: true});
+   // res.Cookie('user', 'admin', {signed: true});
+   req.session.user = 'admin';
     next(); // authorized
   } 
   else {
@@ -63,8 +76,10 @@ function auth (req, res, next) {
   }
  }
  else {
-  if (req.signedCookies.user === 'admin') {
-      next();
+  //if (req.signedCookies.user === 'admin') {
+  if(req.session.user === 'admin'){  
+    console.log('req.session: ', req.session);
+    next();
   }
   else {
       var err = new Error('You are not authenticated!');
