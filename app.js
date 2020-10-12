6ca84ccf1956277b5f2385b2e76bfb1ffd103a91
WardaFarhan <farhan.warda337@gmail.com>
2020-10-12 14:19:28 +0530

    Express Session Part 2

--- app.js
+++ app.js
@@ -43,6 +43,9 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 function auth (req, res, next) {
   //console.log(req.headers);
   //console.log(req.signedCookies);
@@ -50,23 +53,23 @@ function auth (req, res, next) {
 
   //if(!req.signedCookies.user){
   if(!req.session.user){
-   var authHeader = req.headers.authorization;
-  
-   if (!authHeader) {
+  // var authHeader = req.headers.authorization;
+  // if (!authHeader) {
       var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
+      //res.setHeader('WWW-Authenticate', 'Basic');
       err.status = 401;
       next(err);
       return;
   }
 
-  var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+ /* var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
   var user = auth[0];
   var pass = auth[1];
   if (user == 'admin' && pass == 'password') {
    // res.Cookie('user', 'admin', {signed: true});
    req.session.user = 'admin';
-    next(); // authorized
+    next(); // authorized 
+    
   } 
   else {
       var err = new Error('You are not authenticated!');
@@ -74,11 +77,11 @@ function auth (req, res, next) {
       err.status = 401;
       next(err);
   }
- }
+ }*/
  else {
   //if (req.signedCookies.user === 'admin') {
-  if(req.session.user === 'admin'){  
-    console.log('req.session: ', req.session);
+  if(req.session.user === 'authenticated'){  
+    //console.log('req.session: ', req.session);
     next();
   }
   else {
@@ -94,8 +97,7 @@ app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
